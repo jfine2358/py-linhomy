27f3037adb57e76b854f348245818227a814acab
Jonathan Fine <jfine2358@gmail.com>
2016-01-09 15:56:00 +0000

    Add RankMatrices.C_rule and RankMatrices.D_rule.

--- py/linhomy/rankmatrices.py
+++ py/linhomy/rankmatrices.py
@@ -4,7 +4,7 @@ identity_matrices = RankMatrices(identity_rule)
 >>> for n in range(2, 11):
 ...     for m in range(1, n):
 ...         if 2 * m <= n:
-...             print(n, m, identity_matrices.stats(n - m, m))
+...             print(n, m, identity_matrices.product_stats(n - m, m))
 2 1 [(1, 2)]
 3 1 [(0, 3), (1, 3)]
 4 1 [(0, 10), (1, 5)]
@@ -31,6 +31,40 @@ identity_matrices = RankMatrices(identity_rule)
 10 4 [(0, 5639), (1, 139), (2, 6), (3, 1)]
 10 5 [(0, 5542), (1, 144), (2, 10)]
 
+
+>>> C_in_AAA = identity_matrices.C_rule
+
+>>> for d in range(11):
+...     counter = Counter(C_in_AAA[d].flatten())
+...     print(d, sorted(counter.items()))
+0 [(1, 1)]
+1 [(0, 1), (1, 1)]
+2 [(0, 3), (1, 3)]
+3 [(-1, 1), (0, 10), (1, 4)]
+4 [(-1, 2), (0, 31), (1, 7)]
+5 [(-1, 4), (0, 89), (1, 11)]
+6 [(-1, 7), (0, 248), (1, 18)]
+7 [(-1, 12), (0, 673), (1, 29)]
+8 [(-1, 20), (0, 1803), (1, 47)]
+9 [(-1, 33), (0, 4786), (1, 76)]
+10 [(-1, 54), (0, 12639), (1, 123)]
+
+>>> D_in_AAA = identity_matrices.D_rule
+
+>>> for d in range(11):
+...     counter = Counter(D_in_AAA[d].flatten())
+...     print(d, sorted(counter.items()))
+0 [(0, 1), (1, 1)]
+1 [(0, 2), (1, 1)]
+2 [(0, 8), (1, 2)]
+3 [(0, 21), (1, 3)]
+4 [(0, 60), (1, 5)]
+5 [(0, 160), (1, 8)]
+6 [(0, 429), (1, 13)]
+7 [(0, 1134), (1, 21)]
+8 [(0, 2992), (1, 34)]
+9 [(0, 7865), (1, 55)]
+10 [(0, 20648), (1, 89)]
 '''
 
 from __future__ import absolute_import
@@ -45,7 +79,10 @@ import numpy
 from .fibonacci import FIB_WORDS
 from .matrices import fib_zeros_array
 from .matrices import invert_grow_list
+from .matrices import C_in_CD
+from .matrices import CDR_from_CD
 from .matrices import CDR_from_FLAG
+from .matrices import D_in_CD
 from .matrices import IC_from_CDR
 from .product import product_formula
 from .product import change_product_basis
@@ -72,6 +109,36 @@ def _AAA_from_CDR(rule, deg):
     return value
 
 
+def C_in_AAA_factory(CD_from_AAA, AAA_from_CD):
+
+    # Based on C_in_CDR.
+    @grow_list
+    def C_in_AAA(self):
+        deg = len(self)
+        value = fib_zeros_array(deg + 1, deg)
+
+        tmp = numpy.dot(C_in_CD[deg], CD_from_AAA[deg])
+        value = numpy.dot(AAA_from_CD[deg+1], tmp)
+        return value
+
+    return C_in_AAA
+
+
+def D_in_AAA_factory(CD_from_AAA, AAA_from_CD):
+
+    # Based on C_in_AAA_factory.
+    @grow_list
+    def D_in_AAA(self):
+        deg = len(self)
+        value = fib_zeros_array(deg + 2, deg)
+
+        tmp = numpy.dot(D_in_CD[deg], CD_from_AAA[deg])
+        value = numpy.dot(AAA_from_CD[deg+2], tmp)
+        return value
+
+    return D_in_AAA
+
+
 class RankMatrices:
 
     def __init__(self, rule):
@@ -82,6 +149,15 @@ class RankMatrices:
         )
 
         self.CDR_from_AAA = invert_grow_list(self.AAA_from_CDR)
+
+        # Needed for C_in_AAA and D_in_AAA matrices.
+        self.AAA_from_CD = grow_list(
+            lambda self_: numpy.dot(
+                self.AAA_from_CDR[len(self_)],
+                CDR_from_CD[len(self_)]
+            )
+        )
+        self.CD_from_AAA = invert_grow_list(self.AAA_from_CD)
 
         # Needed for change_product_basis.
         self.IC_from_AAA = grow_list(
@@ -99,6 +175,16 @@ class RankMatrices:
             )
         )
 
+        self.C_rule = C_in_AAA_factory(
+            self.CD_from_AAA,
+            self.AAA_from_CD
+        )
+
+        self.D_rule = D_in_AAA_factory(
+            self.CD_from_AAA,
+            self.AAA_from_CD
+        )
+
         self.doit = lambda n, m: change_product_basis(
             product_formula(n, m),
             self.IC_from_AAA[n],
@@ -106,7 +192,7 @@ class RankMatrices:
             self.AAA_from_FLAG[n+m]
         )
 
-        self.stats = lambda n, m: sorted(
+        self.product_stats = lambda n, m: sorted(
             Counter(self.doit(n, m).flatten()).items()
         )
 
