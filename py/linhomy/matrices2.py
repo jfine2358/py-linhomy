83d8a61468db8cff510b98fd2a51486ed5fbd692
Jonathan Fine <jfine2358@gmail.com>
2016-01-30 20:48:27 +0000

    Add iter_pairs(n) to matrices2.py.

--- py/linhomy/matrices2.py
+++ py/linhomy/matrices2.py
@@ -1,7 +1,7 @@
 '''New approach based on rank and {i}
 
 Arises from
-  C^r{CD_1 ... } + {C^rD_1C ...} = {C^rCD_1 ...}
+  C^r{CD_1 ...} + {C^rD_1C ...} = {C^rCD_1 ...}
 
 >>> for chain in iter_chains(6):
 ...     str(' ').join(map(CD_from_word, chain))
@@ -13,6 +13,29 @@ Arises from
 'CDCCCD CCDCCD CCCDCD'
 'CDCCDC CCDCDC'
 'CDDCCC CCDDCC CCCDDC'
+
+
+Arises from
+    C{D_1 ...} + {D_1C ...} = {CD_1 ...}
+
+>>> for chain in iter_pairs(3):
+...     str(' ').join(map(CD_from_word, chain))
+'DC CD'
+
+>>> for chain in iter_pairs(4):
+...     str(' ').join(map(CD_from_word, chain))
+'DCC CDC'
+
+>>> for chain in iter_pairs(5):
+...     str(' ').join(map(CD_from_word, chain))
+'DCCC CDCC'
+'DDC CDD'
+
+>>> for chain in iter_pairs(6):
+...     str(' ').join(map(CD_from_word, chain))
+'DCCCC CDCCC'
+'DCCD CDCD'
+'DDCC CDDC'
 
 '''
 
@@ -56,6 +79,24 @@ def iter_chains(n):
         tmp = split_word(word)
         if tmp:
             yield chain_from_split(*tmp)
+
+
+pair_split_re = re.compile(b'(\x02+)(\x01)((?!\x02).*)')
+def pair_split(word):
+
+    mo = pair_split_re.match(word)
+    if mo:
+        return mo.groups()
+    else:
+        return None
+
+def iter_pairs(n):
+
+    for word in FIB_WORDS[n]:
+        tmp = pair_split(word)
+        if tmp:
+            pre, c, post = tmp
+            yield pre + c + post, c + pre + post
 
 
 if __name__ == '__main__':
